8b429a85ae367d293a1be1b7eee4740a899779d8
dmoetech <github@moesibert.de>
2020-09-07 23:29:09 +0200

    .gitignore fix

--- tmp/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/daniel/Data/Tortuga-Webdesign/Kunden_und_Projekte/0000/mastergoal_project/tmp/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
