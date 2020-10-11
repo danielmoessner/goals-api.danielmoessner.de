52bcbb2a9c764662dc038dac9d1e17a7ee48225d
dmoetech <github@moesibert.de>
2020-10-11 15:40:18 +0200

    removed users edit form views and changed settings view

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from apps.users import viewsets
 from apps.users.views import views
-from apps.users.views import form_views
 
 router = DefaultRouter()
 router.register(r'users', viewsets.UserViewSet)
@@ -14,17 +13,8 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/register/', views.CreateUser.as_view(), name='register'),
     path('api/change-password/', views.ChangePasswordView.as_view(), name='change_password'),
-    path('user/signup/', form_views.CustomUserSignUp.as_view(), name='user_sign_up'),
-    path('user/login/', form_views.CustomUserSignIn.as_view(), name='user_sign_in'),
-    path('user/signout/', form_views.SignOutView.as_view(), name='user_sign_out'),
-    path('user/<pk>/edit', form_views.CustomUserEdit.as_view(), name='user_edit'),
-    path('user/<pk>/password', form_views.CustomUserPassword.as_view(), name='user_password'),
-    path('user/<pk>/todoschoices', form_views.CustomUserToDosChoices.as_view(), name='user_to_dos_choices'),
-    path('user/<pk>/pagechoice', form_views.CustomUserPageChoice.as_view(), name='user_page_choice'),
-    path('user/<pk>/goalviewchoice', form_views.CustomUserGoalViewChoices.as_view(), name='user_goal_view_choices'),
-    path('user/<pk>/treeviewchoices', form_views.CustomUserTreeViewChoices.as_view(), name='user_treeview_choices'),
-    path('user/<pk>/strategy-main-choices/', form_views.UpdateStrategyMainChoicesUser.as_view(),
-         name='update_strategy_main_choice_user'),
+    path('user/login/', views.CustomUserSignIn.as_view(), name='user_sign_in'),
+    path('user/signout/', views.SignOutView.as_view(), name='user_sign_out'),
     # views
     path('signin/', views.SignInView.as_view(), name='sign_in'),
     path('signup/', views.SignUpView.as_view(), name='sign_up'),
--- apps/users/views.py
+++ apps/users/views.py
@@ -8,6 +8,53 @@ from apps.users.models import CustomUser
 from django.shortcuts import redirect
 from rest_framework import generics, status
 from django.views import generic
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.core.exceptions import ImproperlyConfigured
+from django.template.loader import render_to_string
+from django.contrib.auth import login as auth_login
+from django.views import generic
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+
+from apps.users.models import CustomUser
+from apps.users.forms import UserStrategyMainChoiceForm
+from apps.users.forms import CustomUserCreationForm
+from apps.users.forms import CustomUserTreeViewForm
+from apps.users.forms import CustomUserGoalViewForm
+from apps.users.forms import CustomUserChangeForm
+from apps.users.forms import CustomUserToDosForm
+from apps.users.forms import CustomUserPageForm
+from apps.core.views import CustomAjaxFormMixin
+
+import json
+
+
+class SignOutView(LogoutView):
+    pass
+
+
+class CustomUserSignIn(LoginView):
+    template_name = 'snippets/form.j2'
+    redirect_url = reverse_lazy('core:redirect')
+
+    def get_redirect_url(self):
+        if not self.redirect_url:
+            raise ImproperlyConfigured("No URL to redirect to. Provide a redirect_url.")
+        return str(self.redirect_url)
+
+    def form_invalid(self, form):
+        html = render_to_string(self.template_name, self.get_context_data(form=form),
+                                request=self.request)
+        return HttpResponse(json.dumps({"valid": False, "html": html}),
+                            content_type="application/json")
+
+    def form_valid(self, form):
+        auth_login(self.request, form.get_user())
+        return HttpResponse(json.dumps({"valid": True}), content_type="application/json")
 
 
 class CreateUser(generics.CreateAPIView):
