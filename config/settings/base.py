6b1d660d41d83c46ba8617d371bf4d945a700acd
dmoetech <github@moesibert.de>
2020-10-13 23:04:09 +0200

    deleted static dir

--- config/settings/base.py
+++ config/settings/base.py
@@ -140,9 +140,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/dist'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/dist'),
+# ]
 
 STATIC_ROOT = os.path.join(TMP_DIR, 'static')
 
