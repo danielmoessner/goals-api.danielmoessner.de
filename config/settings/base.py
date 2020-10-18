ad7863196476b3b7fc3b8a4381c8ab5b741ae8cb
dmoetech <github@moesibert.de>
2020-10-18 16:26:50 +0200

    removed django templates hack

--- config/settings/base.py
+++ config/settings/base.py
@@ -210,9 +210,3 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "https://goals.danielmoessner.de"
 ]
-
-# HACK ATTACK: this allows Django template tags to span multiple lines.
-import re
-from django.template import base
-
-base.tag_re = re.compile(base.tag_re.pattern, re.DOTALL)
