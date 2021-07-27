218a49f2d560814c970f22ec203e3b6f35d9238f
danielmoessner <github@moesibert.de>
2021-07-27 23:03:39 +0200

    achievements

--- apps/achievements/serializers.py
+++ apps/achievements/serializers.py
@@ -2,7 +2,17 @@ from apps.achievements.models import Achievement
 from rest_framework import serializers
 
 
-class AchievementSerializer(serializers.HyperlinkedModelSerializer):
+class AddUserMixin:
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        if 'user' not in attrs:
+            attrs['user'] = self.context['request'].user
+        return attrs
+
+
+class AchievementSerializer(AddUserMixin, serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Achievement
-        fields = '__all__'
+        exclude = ['user']
