0601ef9709a00fd90f21e7d824a404fb9585ca00
danielmoessner <github@danielmoessner.de>
2024-11-05 05:13:07 +0100

    change logigng

---
+++ call-deploy.sh
@@ -0,0 +1,1 @@
+ssh root@46.101.136.214 /home/goals-api.danielmoessner.de/deploy.sh
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,55 +1,25 @@
 from .base import *
 
 
-# Secret Settings
-
 DEBUG = False
 
 ALLOWED_HOSTS = get_secret("ALLOWED_HOSTS")
 
-
-# Logging
-
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'handlers': {
-        'debug': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(TMP_DIR, 'logs/debug.log'),
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "warning": {
+            "level": "WARNING",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(BASE_DIR, "tmp/django.log"),
         },
-        'info': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(TMP_DIR, 'logs/info.log'),
-        },
-        'warning': {
-            'level': 'WARNING',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(TMP_DIR, 'logs/warning.log'),
-        },
-        'error': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(TMP_DIR, 'logs/error.log'),
-        },
-        'critical': {
-            'level': 'CRITICAL',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(TMP_DIR, 'logs/critical.log'),
-        },
-        'mail_admins': {
-            'level': 'INFO',
-            'class': 'django.utils.log.AdminEmailHandler',
-            'include_html': True,
-        }
     },
-    'loggers': {
-        'django': {
-            'handlers': ['debug', 'info', 'warning', 'error', 'critical', 'mail_admins'],
-            'level': 'DEBUG',
-            'propagate': True,
-        }
-    }
+    "loggers": {
+        "django": {
+            "handlers": ["warning"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
 }
