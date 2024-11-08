261bfbd4f4f072374ee569289c0aec23677c5925
danielmoessner <github@danielmoessner.de>
2024-11-08 09:55:38 +0100

    improve structure

--- apps/todos/forms.py
+++ apps/todos/forms.py
@@ -1,32 +1,47 @@
-from typing import Any
+from typing import Any, Generic, TypeVar
 from django import forms
 
-from apps.todos.mixins import GetInstance
-from apps.todos.models import NeverEndingTodo, NormalTodo, RepetitiveTodo, Todo
+from apps.todos.models import NeverEndingTodo, NormalTodo, PipelineTodo, RepetitiveTodo, Todo
 from django.utils import timezone
-
-from apps.todos.utils import add_week, get_datetime_widget, get_end_of_week, get_specific_todo, get_start_of_week, setup_duration_field
+from django.db import models
+from apps.todos.utils import add_week, get_datetime_widget, get_end_of_week, get_specific_todo, get_start_of_week, setup_datetime_field, setup_duration_field
 from apps.users.models import CustomUser
 from django.contrib.auth.models import AbstractBaseUser, AnonymousUser
 
 USER = AbstractBaseUser | AnonymousUser | CustomUser
 OPTS = dict[str, Any]
+T = TypeVar("T", bound=models.Model)
 
 
-class CreateNormalTodo(GetInstance[NormalTodo], forms.ModelForm):
+class OptsUserInstance(Generic[T]):
+    instance: T
+
+    def init(self):
+        pass
+    
+    def get_instance(self) -> models.Model | None:
+        return None
+
+    def __init__(self, user: USER, opts: OPTS, *args, **kwargs):
+        assert isinstance(user, CustomUser)
+        self.user = user
+        self.opts = opts
+        instance = self.get_instance()
+        super().__init__(*args, instance=instance, **kwargs)  # type: ignore
+        self.init()
+
+
+class CreateNormalTodo(OptsUserInstance[NormalTodo], forms.ModelForm):
     nav = "create"
 
     class Meta:
         model = NormalTodo
         fields = ["name", "activate", "deadline"]
 
-    def __init__(self, user: USER, opts: OPTS, *args, **kwargs):
-        assert isinstance(user, CustomUser)
-        self.user = user
-        super().__init__(*args, **kwargs)
+    def init(self):
         self.fields["activate"].widget = get_datetime_widget()
         self.fields["deadline"].widget = get_datetime_widget()
-        variant = opts.get("variant", "this_week")
+        variant = self.opts.get("variant", "this_week")
         if variant == "this_week":
             self.fields["activate"].initial = get_start_of_week()
             self.fields["deadline"].initial = get_end_of_week()
@@ -40,7 +55,7 @@ class CreateNormalTodo(GetInstance[NormalTodo], forms.ModelForm):
         return self.instance.pk
 
 
-class CreateNeverEndingTodo(GetInstance[NeverEndingTodo], forms.ModelForm):
+class CreateNeverEndingTodo(OptsUserInstance[NeverEndingTodo], forms.ModelForm):
     nav = "create"
     text = "A never ending todo will reappear after the completion date + the duration time."
     submit = "Create"
@@ -49,10 +64,7 @@ class CreateNeverEndingTodo(GetInstance[NeverEndingTodo], forms.ModelForm):
         model = NeverEndingTodo
         fields = ["name", "duration"]
 
-    def __init__(self, user: USER, opts: OPTS, *args, **kwargs):
-        assert isinstance(user, CustomUser)
-        self.user = user
-        super().__init__(*args, **kwargs)
+    def init(self):
         setup_duration_field(self.fields["duration"])
 
     def ok(self):
@@ -62,7 +74,7 @@ class CreateNeverEndingTodo(GetInstance[NeverEndingTodo], forms.ModelForm):
         return self.instance.pk
 
 
-class CreateRepetitiveTodo(GetInstance[RepetitiveTodo], forms.ModelForm):
+class CreateRepetitiveTodo(OptsUserInstance[RepetitiveTodo], forms.ModelForm):
     nav = "create"
     submit = "Create"
 
@@ -70,10 +82,7 @@ class CreateRepetitiveTodo(GetInstance[RepetitiveTodo], forms.ModelForm):
         model = RepetitiveTodo
         fields = ["name", "duration", "repetitions"]
     
-    def __init__(self, user: USER, opts: OPTS, *args, **kwargs):
-        assert isinstance(user, CustomUser)
-        self.user = user
-        super().__init__(*args, **kwargs)
+    def init(self):
         setup_duration_field(self.fields["duration"])
 
     def ok(self):
@@ -84,63 +93,71 @@ class CreateRepetitiveTodo(GetInstance[RepetitiveTodo], forms.ModelForm):
         return self.instance.pk
 
 
-class UpdateRepetitiveTodo(GetInstance[RepetitiveTodo], forms.ModelForm):
+class CreatePipelineTodo(OptsUserInstance[PipelineTodo], forms.ModelForm):
+    nav = "create"
+    submit = "Create"
+    text = "A pipeline todo activates once its previous todo completes"
+
+    class Meta:
+        model = PipelineTodo
+        fields = ["name", "previous"]
+
+    def init(self):
+        qs = Todo.objects.filter(status="ACTIVE", user=self.user).order_by("name")
+        self.fields["previous"].queryset = qs # type: ignore
+
+
+class UpdateRepetitiveTodo(OptsUserInstance[RepetitiveTodo], forms.ModelForm):
     class Meta:
         model = RepetitiveTodo
         fields = ["name", "status", "activate", "deadline", "repetitions"]
 
-    def get_instance(self, pk: str, user: USER):
-        return RepetitiveTodo.objects.get(pk=pk, user=user)
-
-    def __init__(self, user: USER, opts: OPTS, *args, **kwargs):
-        instance = self.get_instance(opts["pk"], user)
-        super().__init__(*args, instance=instance, **kwargs)
-        self.fields["activate"].widget = get_datetime_widget()
-        self.fields["deadline"].widget = get_datetime_widget()
+    def get_instance(self):
+        return RepetitiveTodo.objects.get(pk=self.opts["pk"], user=self.user)
+    
+    def init(self):
+        setup_datetime_field(self.fields["activate"])
+        setup_datetime_field(self.fields["deadline"])
 
     def ok(self) -> int:
         self.instance.save()
         return self.instance.pk
     
 
-class UpdateNormalTodo(GetInstance[NormalTodo], forms.ModelForm):
+class UpdateNormalTodo(OptsUserInstance[NormalTodo], forms.ModelForm):
     class Meta:
         model = NormalTodo
         fields = ["name", "status", "activate", "deadline"]
 
-    def get_instance(self, pk: str, user: USER):
-        return NormalTodo.objects.get(pk=pk, user=user)
+    def get_instance(self):
+        return NormalTodo.objects.get(pk=self.opts["pk"], user=self.user)
 
-    def __init__(self, user: USER, opts: OPTS, *args, **kwargs):
-        instance = self.get_instance(opts["pk"], user)
-        super().__init__(*args, instance=instance, **kwargs)
-        self.fields["activate"].widget = get_datetime_widget()
-        self.fields["deadline"].widget = get_datetime_widget()
+    def init(self):
+        setup_datetime_field(self.fields["activate"])
+        setup_datetime_field(self.fields["deadline"])
 
     def ok(self) -> int:
         self.instance.save()
         return self.instance.pk
 
 
-class UpdateNeverEndingTodo(GetInstance[NeverEndingTodo], forms.ModelForm):
+class UpdateNeverEndingTodo(OptsUserInstance[NeverEndingTodo], forms.ModelForm):
     class Meta:
         model = NeverEndingTodo
         fields = ["name", "status", "activate", "duration"]
     
-    def get_instance(self, pk: str, user: USER):
-        return NeverEndingTodo.objects.get(pk=pk, user=user)
-    
-    def __init__(self, user: USER, opts: OPTS, *args, **kwargs):
-        instance = self.get_instance(opts["pk"], user)
-        super().__init__(*args, instance=instance, **kwargs)
-        self.fields["activate"].widget = get_datetime_widget()
+    def get_instance(self):
+        return NeverEndingTodo.objects.get(pk=self.opts["pk"], user=self.user)
+
+    def init(self):
+        setup_datetime_field(self.fields["activate"])
     
     def ok(self) -> int:
         self.instance.save()
         return self.instance.pk
 
 
-class DeleteTodo(GetInstance[NormalTodo], forms.ModelForm):
+class DeleteTodo(OptsUserInstance[NormalTodo], forms.ModelForm):
     text = "Are you sure you want to delete this todo?"
     submit = "Delete"
 
@@ -148,26 +165,22 @@ class DeleteTodo(GetInstance[NormalTodo], forms.ModelForm):
         model = Todo
         fields = []
     
-    def __init__(self, user: USER, opts: OPTS, *args, **kwargs):
-        assert isinstance(user, CustomUser)
-        instance = get_specific_todo(pk=opts["pk"], user=user)
-        super().__init__(*args, instance=instance, **kwargs)
+    def get_instance(self):
+        return get_specific_todo(pk=self.opts["pk"], user=self.user)
 
     def ok(self) -> int:
         self.instance.delete()
         return 0
 
 
-class ToggleTodo(GetInstance[Todo], forms.ModelForm):
+class ToggleTodo(OptsUserInstance[Todo], forms.ModelForm):
     class Meta:
         model = Todo
         fields = []
-
-    def __init__(self, user: USER, opts: OPTS, *args, **kwargs):
-        assert isinstance(user, CustomUser)
-        instance = get_specific_todo(pk=opts["pk"], user=user)
-        super().__init__(*args, instance=instance, **kwargs)
     
+    def get_instance(self):
+        return get_specific_todo(pk=self.opts["pk"], user=self.user)
+
     def ok(self) -> int:
         self.instance.toggle()
         self.instance.save()
