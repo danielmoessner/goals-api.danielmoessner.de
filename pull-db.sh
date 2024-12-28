52ea7e8e1b67b6c3ec11623b0f43d1060642615f
danielmoessner <github@danielmoessner.de>
2024-12-28 14:25:27 +0100

    change domain to goals

--- pull-db.sh
+++ pull-db.sh
@@ -1,1 +1,1 @@
-scp root@46.101.136.214:/home/goals-api.danielmoessner.de/tmp/db.sqlite3 ./tmp/db.sqlite3
+scp root@46.101.136.214:/home/goals.danielmoessner.de/tmp/db.sqlite3 ./tmp/db.sqlite3
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 cd /home/
-git clone https://github.com/danielmoessner/goals-api.danielmoessner.de.git
-cd goals-api.danielmoessner.de
+git clone https://github.com/danielmoessner/goals.danielmoessner.de.git
+cd goals.danielmoessner.de
 mkdir tmp
 mkdir tmp/logs
 mkdir tmp/media
 mkdir tmp/static
 touch tmp/logs/django.log
-ln -s /home/goals-api.danielmoessner.de/apache.conf /etc/apache2/sites-available/goals-api.danielmoessner.de.conf
+ln -s /home/goals.danielmoessner.de/apache.conf /etc/apache2/sites-available/goals.danielmoessner.de.conf
